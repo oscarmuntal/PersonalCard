05b1174c9ccd7c052a6b8ce47f7b78dd9be823b9
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-31 13:12:29 +0200

    Initial Commit

---
+++ OscarCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OscarCard
+//
+//  Created by Òscar Muntal on 31/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OscarCard/OscarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OscarCardApp.swift
+//  OscarCard
+//
+//  Created by Òscar Muntal on 31/3/23.
+//
+
+import SwiftUI
+
+@main
+struct OscarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
