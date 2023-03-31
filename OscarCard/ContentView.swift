b76a6c04c2078fb03f9b1b94c6256d9fa8b4ac3c
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-31 16:55:37 +0200

    Telephone section extracted as the reusable view InfoView

--- OscarCard/ContentView.swift
+++ OscarCard/ContentView.swift
@@ -29,22 +29,32 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .font(.system(size: 25))
                 Divider()
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color.white)
-                    .frame(height: 50)
-                    .overlay(
-                        HStack {
-                            Image(systemName: "phone.fill")
-                                .foregroundColor(.green)
-                            Text("+34 123 456 789")
-                        }
-                    )
+                InfoView(text: "+34 123 456 789", imageName: "phone.fill")
 
-                
             }
         }
     }
 }
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                }
+            )
+            .padding(.all)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
