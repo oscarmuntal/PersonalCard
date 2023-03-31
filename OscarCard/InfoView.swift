c8a7ca7a9919cf0ecfbe071f9b976e9f23a230d1
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-31 17:59:09 +0200

    Fixing text to black color so it doesn't change in dark mode

--- OscarCard/InfoView.swift
+++ OscarCard/InfoView.swift
@@ -20,6 +20,7 @@ struct InfoView: View {
                     Image(systemName: imageName)
                         .foregroundColor(.green)
                     Text(text)
+                        .foregroundColor(Color.black)
                 }
             )
             .padding(.all)
