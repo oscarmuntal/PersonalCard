084cb1cd9fc46e63597e84fa45cbb7d44e229c7f
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-31 13:20:32 +0200

    Adding background color in a ZStack ignoring the safe area edges

--- OscarCard/ContentView.swift
+++ OscarCard/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.green)
+                .edgesIgnoringSafeArea(.all)
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
